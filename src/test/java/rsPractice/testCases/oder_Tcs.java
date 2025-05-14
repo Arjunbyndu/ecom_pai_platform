494c9985df53ac0e4021e3980eada58682ed631a
Arjun <arjunbyndoorkar@gmail.com>
2025-05-14 19:48:55 +0530

    oder with offer

--- src/test/java/rsPractice/testCases/oder_Tcs.java
+++ src/test/java/rsPractice/testCases/oder_Tcs.java
@@ -29,5 +29,22 @@ public class oder_Tcs {
 		JavascriptExecutor js = ((JavascriptExecutor)driver);
 		js.executeScript("window.scrollTo(0,700)");
 		driver.findElement(By.xpath("(//div[@class='projects-item']/a)[1]")).click();
-	}		
+	}	
+	
+	public  void orderWithOffer() throws InterruptedException
+	{
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		
+		driver.get("https://rahulshettyacademy.com/practice-project");
+		driver.findElement(By.xpath("//input[@placeholder='Your Name*']")).sendKeys("Arjun");
+		driver.findElement(By.xpath("//input[@placeholder='Your Email*']")).sendKeys("arjunbyndu@gmail.com");
+		driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+		driver.findElement(By.cssSelector("#form-submit:nth-of-type(1)")).click();
+		
+	}	
 }
